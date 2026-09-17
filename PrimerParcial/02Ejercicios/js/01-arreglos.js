0c9d50f08e099e8089afa66c9401bfece50dfcb9
damnsic <dnerio2400@alumno.ipn.mx>
2026-09-17 16:51:50 -0600

    Avance de funciones y tarea pendiente 17/09/26

---
+++ PrimerParcial/02Ejercicios/js/01-arreglos.js
@@ -0,0 +1,22 @@
+const talleres = [
+    { nombre: 'Introducción a Python', instructor: 'Ing. María López', cupo: 25, inscritos: 25 }, 
+    { nombre: 'Fundamentos de Redes', instructor: 'Ing. Carlos Ramírez', cupo: 30, inscritos: 18 },
+    { nombre: 'Diseño de Bases de Datos', instructor: 'Ing. Ana Torres', cupo: 20, inscritos: 20 },
+    { nombre: 'Desarrollo Web con JavaScript', instructor: 'Ing. María López', cupo: 25, inscritos: 10 }      
+];
+
+// TODO: forEach - imprime "- <nombre> (<inscritos>/<cupo>)" de cada taller
+console.log("Aplicando un forEach para imprimir los talleres:");
+talleres.forEach((t) => console.log(`- ${t.nombre}(${t.inscritos}/${t.cupo})`));
+
+// TODO: map - crea un arreglo `nombres` solo con los nombres de los talleres
+console.log("Aplicando funcion Map con solo Nombres:"); 
+const nombres = talleres.map((t) => t.nombre);
+console.log(nombres);
+
+// TODO: filter - crea un arreglo `llenos` con los talleres donde inscritos >= cupo
+console.log("Aplicando la funcion Filter en los talleres:");
+const llenos = talleres.filter((t) => t.inscritos >= t.cupo);
+console.log(llenos.map((t) => t.nombre));
+
+// TODO: find - encuentra el primer taller impartido por 'Ing. María López'
